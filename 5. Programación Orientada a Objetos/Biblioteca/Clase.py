c00987440fac5795d522418812777ad1feb604aa
panque-buap <rafael.garciasan@correo.buap.mx>
2022-11-07 19:05:08 -0600

    Actualizacion 7 de noviembre de 2022 incluyendo temas de estructuras de datos no lineales, ciencia de datos y logica

---
+++ 5. Programación Orientada a Objetos/Biblioteca/Clase.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Algoritmos_Clasificacion1:
+    def Algoritmo1_C1(self):
+        print ("Algoritmo 1 de Algoritmos Clasificacion 1")
+    def Algoritmo2_C1(self):
+        print ("Algoritmo 2 de Algoritmos Clasificacion 1")
+
+class Algoritmos_Clasificacion2:
+    def Algoritmo1_C2(self):
+        print ("Algoritmo 1 de Algoritmos Clasificacion 2")
+    def Algoritmo2_C2(self):
+        print ("Algoritmo 2 de Algoritmos Clasificacion 2")
+    def Algoritmo3_C2(self):
+        print ("Algoritmo 2 de Algoritmos Clasificacion 2")
+
+class Algoritmos_Clasificacion3:
+    def Algoritmo1_C3(self):
+        print ("Algoritmo 1 de Algoritmos Clasificacion 3")
+
+
+# In[ ]:
+
+
+
+
